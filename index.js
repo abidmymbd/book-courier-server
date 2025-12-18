daddc069a9e6e37f92618560f2f3efc0f7f41721
Abid Hasan <youremail@example.com>
2025-12-19 04:25:10 +0600

    added domain site

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server
-        await client.connect();
+        // await client.connect();
 
         const db = client.db('book_courier_db')
         const booksCollection = db.collection('books')
@@ -482,8 +482,8 @@ async function run() {
 
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // await client.close();
     }
